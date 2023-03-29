4d302c2bc34638ebb7d0c9e65b04c63a4d58989b
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-03-29 15:53:29 +0300

    api support added, components added

--- src/App.js
+++ src/App.js
@@ -1,48 +1,44 @@
 import './css/App.css';
+import React from 'react';
+import Login from './component/login';
+import Register from './component/register';
 
-function App() {
-  return (
-    <div className="App">
-      
-      <br></br>
-      <br></br>
-      <br></br>
+class App  extends React.Component{
 
-      <div className='row'>
-      <div className='col-md-4'></div>
-      
-      <div className='col-md-4'>
-          <div className='row auth' align="center">
-            <div className='col-md-6'>
-                <p>Login</p>
+
+  render() {
+    return (
+      <div className="App">
+        
+        <br></br>
+        <br></br>
+        <br></br>
+  
+        <div className='row'>
+        <div className='col-md-4'></div>
+        
+        <div className='col-md-4'>
+            <div className='row auth' align="center">
+              <div className='col-md-6'>
+                  <p>Login</p>
+                  <hr></hr>
+              </div>
+  
+              <div className='col-md-6'>
+                <p>Sign up</p>
                 <hr></hr>
-            </div>
-
-            <div className='col-md-6'>
-              <p>Sign up</p>
-              <hr></hr>
-            </div>
-
-            <form align="left">
-              <div className="form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
-                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
               </div>
-              <div className="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
-              </div>
-              <br></br>
-              <button type="submit" className="btn btn-success btn_submit">Submit</button>
-            </form>
-            
-          </div>
+  
+              <Login/>
+              <Register />
+              
+            </div>
+        </div>
+        <div className='col-md-4'></div>
+        </div>
       </div>
-      <div className='col-md-4'></div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/component/register.js
@@ -0,0 +1,69 @@
+import React from 'react';
+class Register extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: null,
+        };
+    }
+    
+    
+      handleLogin(e) {
+        e.preventDefault();
+        let email_address = e.target.email_address.value;
+        let password = e.target.password.value;
+    
+        const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ email: email_address, password: password })
+        };
+    
+        fetch("http://localhost:8000/user/register/", requestOptions)
+        .then(res => res.json())
+        .then(
+            (result) => {
+                let status = result['status'];
+                let message = result["message"];
+                if (status == 202) {
+                    //accepted
+                    let username = result["username"];
+                    this.setState({
+                      username: username,
+                    })
+                } else {
+                  // show error message here!
+                }
+            },
+            // Note: it's important to handle errors here
+            // instead of a catch() block so that we don't swallow
+            // exceptions from actual bugs in components.
+            (error) => {
+                console.log(error);
+            }
+        )
+      }
+
+      render() {
+        return(
+            <form align="left" onSubmit={this.handleLogin.bind(this)}>
+                <div className="form-group">
+                  <label for="exampleInputEmail1">Email address</label>
+                  <input type="email" name="email_address" 
+                        className="form-control" id="exampleInputEmail1"
+                        aria-describedby="emailHelp" placeholder="Enter email"/>
+                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div className="form-group">
+                  <label for="exampleInputPassword1">Password</label>
+                  <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <br></br>
+                <button type="submit" className="btn btn-primary btn_submit">Register</button>
+              </form>
+        )
+      }
+}
+
+export default Register;
