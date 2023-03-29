35feb8801a8a930fd46aee38bcd7fabee88a1054
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-03-29 16:12:45 +0300

    cookie support initiated

--- src/component/login.js
+++ src/component/login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Cookies from 'js-cookie';
 class Login extends React.Component {
 
     constructor(props) {
@@ -7,19 +8,27 @@ class Login extends React.Component {
             username: null,
         };
     }
-    
-    
-      handleLogin(e) {
+
+    setCookie() {
+        Cookies.set('username', this.state.username, {
+            path: '/',
+            sameSite: 'strict',
+            maxAge: '10000',
+        })
+    }
+
+
+    handleLogin(e) {
         e.preventDefault();
         let email_address = e.target.email_address.value;
         let password = e.target.password.value;
-    
+
         const requestOptions = {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ email: email_address, password: password })
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: email_address, password: password })
         };
-    
+
         fetch("http://localhost:8000/user/login/", requestOptions)
         .then(res => res.json())
         .then(
@@ -30,10 +39,11 @@ class Login extends React.Component {
                     //accepted
                     let username = result["username"];
                     this.setState({
-                      username: username,
+                        username: username,
                     })
+                    this.setCookie();
                 } else {
-                  // show error message here!
+                    // show error message here!
                 }
             },
             // Note: it's important to handle errors here
@@ -43,27 +53,27 @@ class Login extends React.Component {
                 console.log(error);
             }
         )
-      }
+    }
 
-      render() {
+    render() {
         return(
             <form align="left" onSubmit={this.handleLogin.bind(this)}>
                 <div className="form-group">
-                  <label for="exampleInputEmail1">Email address</label>
-                  <input type="email" name="email_address" 
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" name="email_address" 
                         className="form-control" id="exampleInputEmail1"
                         aria-describedby="emailHelp" placeholder="Enter email"/>
-                  <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
-                  <label for="exampleInputPassword1">Password</label>
-                  <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" name="password" className="form-control" id="exampleInputPassword1" placeholder="Password"/>
                 </div>
                 <br></br>
                 <button type="submit" className="btn btn-success btn_submit">Login</button>
-              </form>
+                </form>
         )
-      }
+    }
 }
 
 export default Login;
