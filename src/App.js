e8fb99ac1a7f665f57c12524dd3243be0d9e8be5
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-03-29 16:41:56 +0300

    cookie support added on landing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import './css/App.css';
-import React from 'react';
-import Login from './component/login';
-import Register from './component/register';
+import "./css/App.css";
+import React from "react";
+import Login from "./component/login";
+import Register from "./component/register";
+import Cookies from "js-cookie";
 
-class App  extends React.Component{
-
-
+class App extends React.Component {
   render() {
+    let username = Cookies.get("username");
+
     return (
       <div className="App">
-        
         <br></br>
         <br></br>
         <br></br>
-  
-        <div className='row'>
-        <div className='col-md-4'></div>
-        
-        <div className='col-md-4'>
-            <div className='row auth' align="center">
-              <div className='col-md-6'>
-                  <p>Login</p>
-                  <hr></hr>
+
+        {username == null && (
+          <div className="container landing">
+            <div className="row">
+              <div className="col-md-4"></div>
+
+              <div className="col-md-4">
+                <div className="row auth" align="center">
+                  <div className="col-md-6">
+                    <p>Login</p>
+                    <hr></hr>
+                  </div>
+
+                  <div className="col-md-6">
+                    <p>Sign up</p>
+                    <hr></hr>
+                  </div>
+
+                  <Login />
+                  <Register />
+                </div>
               </div>
-  
-              <div className='col-md-6'>
-                <p>Sign up</p>
-                <hr></hr>
-              </div>
-  
-              <Login/>
-              <Register />
-              
+              <div className="col-md-4"></div>
             </div>
-        </div>
-        <div className='col-md-4'></div>
-        </div>
+          </div>
+        )}
+
+        {username != null && (
+          <div className="container">
+            <h1> Hello user!</h1>
+          </div>
+        )}
       </div>
     );
   }
