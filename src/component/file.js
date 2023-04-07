7af97d30f3307d2ebbc1c8478efeaba231aea69f
Shepherd <10481417+shepherd-06@users.noreply.github.com>
2023-04-07 17:23:19 +0300

    File download option added

--- src/component/file.js
+++ src/component/file.js
@@ -1,11 +1,44 @@
 import React from "react";
 
 class SingleFileView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      file_id: this.props.item["file_id"],
+      filename: this.props.item["original_filename"],
+    };
+  }
+
+  handleDownload(event) {
+    const download_url =
+      "http://127.0.0.1:8000/share/download/cde7f0fb/" + this.state.file_id;
+
+      fetch(download_url).then((response) => {
+        if (!response.ok) {
+          // console.log(response.body);
+          throw new Error(response.statusText);
+        } else {
+          return response.blob()
+        }
+      }).then((blob) => {
+        if (blob["type"] === "application/json") {
+          //do something here
+          console.log("error occurred!")
+          return
+        }
+        let url = window.URL.createObjectURL(blob);
+        let a = document.createElement("a");
+        console.log(url);
+        a.href = url;
+        a.download = this.state.filename;
+        a.click();
+      }).catch((error) => {
+        console.log('error: ' + error);
+      });
+  }
+
   render() {
     const item = this.props.item;
-
-    const filename = item["original_filename"];
-    const filepath = "http://127.0.0.1:8000/media/" + item["file_path"];
     const uploaded_at = item["uploaded_at"];
 
     return (
@@ -13,7 +46,7 @@ class SingleFileView extends React.Component {
         <div className="row file_item">
           <div className="col-md-8">
             <p align="left" className="h6">
-              {filename}
+              {this.state.filename}
             </p>
             <small align="left">Uploaded at: {uploaded_at}</small>
           </div>
@@ -22,8 +55,7 @@ class SingleFileView extends React.Component {
               <div className="col-sm-4">
                 <i
                   className="gg-software-download vertical-center"
-                  type="submit"
-                  download={filename}
+                  onClick={this.handleDownload.bind(this)}
                 ></i>
               </div>
 
